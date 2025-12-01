c057eaa006bb52372a062bab45b90ac033297107
Dory <78720364+dooory@users.noreply.github.com>
2025-12-01 20:06:02 +1300

    add GameBoard object

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ function Player(name, id) {
 
 	return { getName, getId, doMove };
 }
+
+const GameBoard = (() => {
+	let board = [];
+
+	const getBoard = () => board;
+
+	const doMove = (position, playerId) => {
+		board[position] = playerId;
+
+		return board;
+	};
+
+	const resetBoard = () => {
+		board = Array(9).fill("", 0, 9);
+	};
+
+	return { doMove, getBoard, resetBoard };
+})();
+
+const player1 = Player("Player 1", 1);
+const player2 = Player("Player 2", 2);
